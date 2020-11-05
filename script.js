ddf348062600c44cfb63a286c0e78d47de9cbffe
HamzahBahar <47035954+hMZhbhr@users.noreply.github.com>
2020-11-05 17:07:04 +0200

    Add color/uncolor functionality #6

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ document.querySelector("#create").addEventListener("click",function(){
     createGrid();
 });   
 
+let isSketching = false;
+let isErasing = false;
+// Add event listener (keydown) to the document to capture (s) and (e) keys in addtion to 
+// (Enter) key
+document.addEventListener("keydown",function(event){
+    if(event.code ==="KeyS"){
+        isSketching = !isSketching;
+        isErasing = false;
+    }
+    if(event.code === "Enter"){
+        createGrid();
+        document.querySelector("#rows").blur(); 
+        document.querySelector("#cols").blur();
+    }
+    if(event.code === "KeyE"){
+       isErasing = !isErasing; 
+       isSketching = false;
+    }
+}); 
 
 
-
-
-
-
+// Mouse Over Event
+// Add event listener to the grid container (this will include all the cells inside it)
+gridContainer.addEventListener("mouseover",function(event){
+    if(isSketching){
+        let id = event.target.id;
+        let cellBackgroundColor = getComputedStyle(event.target).getPropertyValue("background-color"); 
+        if(cellBackgroundColor === "rgb(255, 255, 255)"){
+            let randColor = randomColor(); 
+            event.target.style.backgroundColor = convertRGBtoHEX(randColor);
+        }
+    }if(isErasing){
+        erase(event);
+    }
+    
+});
 
 // This function creates the grid by getting the rows and cols
 // then populate it with cells
@@ -41,4 +71,28 @@ function populateCells(numberOfCells){
         gridItem.setAttribute("id",i);
         gridContainer.appendChild(gridItem);
     }  
+} 
+
+// This function generates random color
+// in RGB format
+function randomColor(){
+    let color = {}; 
+    color.red = Math.floor(Math.random()*240); 
+    color.green = Math.floor(Math.random()*240); 
+    color.blue = Math.floor(Math.random()*240);
+    return color; 
+}  
+
+// This function convert RGB color to HEX Code
+function convertRGBtoHEX(color){
+    let zeroPad = (colorPortion)=> colorPortion.length===1 ? "0"+colorPortion : colorPortion;
+    return "#"+zeroPad(color.red.toString(16))+zeroPad(color.green.toString(16))+zeroPad(color.blue.toString(16)); 
+}
+
+// This function erases a cell when being pointed at. 
+function erase(event){ 
+    let cellBackgroundColor = getComputedStyle(event.target).getPropertyPriority("background-color"); 
+    if(cellBackgroundColor !== "rgb(255, 255, 255)"){
+        event.target.style.backgroundColor = convertRGBtoHEX({red:255,green:255,blue:255});
+    }
 }
